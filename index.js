3fa973f0c73be09401df73b96dd99fc97d1f6626
mudasirshah434 <mudasirshah9777@gmail.com>
2024-07-05 15:45:50 +0500

    first comment

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-require('donenv').config()
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000  // Use the environment variable PORT, or default to 3000 if not set
+
+const githubData = {
+  "message": "Not Found",
+  "documentation_url": "https://docs.github.com/rest",
+  "status": "404"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -12,10 +18,14 @@ app.get('/twitter', (req, res) => {
   res.send('Mudasir Shah')
 })
 
-app.get('/login', (req, res)=> {
+app.get('/login', (req, res) => {
   res.send('<h1>Please Login</h1>')
 })
 
-app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+app.get('/github', (req, res) => {
+  res.json(githubData)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)  // Correctly use the 'port' variable here
 })
